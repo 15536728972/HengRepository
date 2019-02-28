5fd5f5a234266e7d0907d6fde878208350d005cf
zhangheng <657805934@qq.com>
2019-02-28 15:49:30 +0800

    删除

--- day03_crawler_demo/src/main/java/com/itheima/dao/ItemDao.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.dao;
-
-import com.itheima.domain.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemDao extends JpaRepository<Item,Long> {
-
-
-}
